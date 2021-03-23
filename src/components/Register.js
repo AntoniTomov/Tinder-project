7ef7ea00fedef2130ab590567d2f6dfa08680bc5
Antoni Tomov <antotomov@gmail.com>
2021-03-23 22:36:04 +0200

    adding show/hide password icon on register form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,36 +1,43 @@
 import React, { useState } from 'react';
-import { CssBaseline, Typography, Container, TextField, Label, makeStyles, Button } from "@material-ui/core";
+import { CssBaseline, Typography, InputLabel, OutlinedInput, FormControl, Container, TextField, Label, IconButton, InputAdornment, makeStyles, Button } from "@material-ui/core";
+import { Visibility, VisibilityOff } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
     root: {
       '& > *': {
         margin: theme.spacing(1),
-        width: '25ch',
+        width: '30ch',
         },
     },
-    flex: {
-        flexGrow: 1,
+    container: {
+        marginTop: '5rem',
+        width: 'max-content',
         margin: theme.spacing(1),
-        width: '25ch',
+        display: 'flex',
+        maxWidth: '400px',
+    },
+    label: {
+        lineHeight: '4px',
     },
 }));
 
 export default function Register(props) {
     const classes = useStyles();
-    const [usernameInput, changeUserNameInput] = useState("");
-    const [passInput, changePassInput] = useState("");
-    const [repeatPassInput, changeRepeatPassInput] = useState("");
-    const [ageInput, changeAgeInput] = useState("");
+    const [usernameInput, setUsernameInput] = useState("");
+    const [passInput, setPassInput] = useState("");
+    const [repeatPassInput, setRepeatPassInput] = useState("");
+    const [ageInput, setAgeInput] = useState("");
+    const [isPassVisible, setIsPassVisible] = useState(true);
 
     function changeInput(ev, field) {
         switch(field) {
-            case 'username': changeUserNameInput(ev.target.value);
+            case 'username': setUsernameInput(ev.target.value);
             break;
-            case 'password': changePassInput(ev.target.value);
+            case 'password': setPassInput(ev.target.value);
             break;
-            case 'repeatPassword': changeRepeatPassInput(ev.target.value);
+            case 'repeatPassword': setRepeatPassInput(ev.target.value);
             break;
-            case 'age': changeAgeInput(ev.target.value);
+            case 'age': setAgeInput(ev.target.value);
             break;
             default:
         }
@@ -46,15 +53,20 @@ export default function Register(props) {
         }
         console.log('From Register.js: ', user);
         props.regUser(user);
-        changeUserNameInput('');
-        changePassInput('');
-        changeRepeatPassInput('');
-        changeAgeInput('');
+        setUsernameInput('');
+        setPassInput('');
+        setRepeatPassInput('');
+        setAgeInput('');
     }
 
-    function validateField() {
+    const handleClickShowPassword = () => {
+        isPassVisible ? setIsPassVisible(false) : setIsPassVisible(true);
+        console.log(isPassVisible)
+    };
 
-    }
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
 
 
 
@@ -62,22 +74,72 @@ export default function Register(props) {
     return (
         <>
         <CssBaseline />
-        <Container className={classes.flex} position="relative" align="center" gutterBottom>
+        <Container className={classes.container} position="relative" align="center" gutterBottom>
             <form className={classes.root} noValidate autoComplete="off">
-                <TextField id="username" autoFocus error={usernameInput.length < 4 && usernameInput.length !== 0} value={usernameInput} onChange={(ev) => changeInput(ev, 'username')} label="Username" variant="outlined" margin="dense" />
-                <TextField id="password" error={usernameInput.length < 4 && usernameInput.length !== 0} value={passInput} onChange={(ev) => changeInput(ev, 'password')}  label="Password" variant="outlined" margin="dense" type="password" autoComplete="current-password" />
-                <TextField id="repeatPassword" error={usernameInput.length < 4 && usernameInput.length !== 0} value={repeatPassInput} onChange={(ev) => changeInput(ev, 'repeatPassword')}
-                label="Re-type password"
-                variant="outlined"
-                margin="dense"
-                type="password"
-                autoComplete="current-password" />
-                <TextField id="age" error={usernameInput.length < 4 && usernameInput.length !== 0} value={ageInput} onChange={(ev) => changeInput(ev, 'age')}
-                label="How old are you?"
-                variant="outlined"
-                margin="dense"
-                type="number" />
-
+                <TextField
+                    id="username"
+                    autoFocus
+                    error={usernameInput.trim().length < 4 && usernameInput.length !== 0} value={usernameInput}
+                    onChange={(ev) => changeInput(ev, 'username')}
+                    label="Username" variant="outlined" />
+                <FormControl variant="outlined">
+                    <InputLabel className="label" htmlFor="password">Password</InputLabel>
+                    <OutlinedInput
+                        id="password"
+                        error={passInput.trim().length < 4 && passInput.length !== 0} 
+                        value={passInput} 
+                        onChange={(ev) => changeInput(ev, 'password')}  
+                        label="Password"
+                        variant="outlined"
+                        type={isPassVisible ? 'text' : 'password'}
+                        autoComplete="current-password"
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                {isPassVisible ? <Visibility /> : <VisibilityOff />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                    />
+                </FormControl>
+                <FormControl variant="outlined">
+                    <InputLabel className="label" htmlFor="repeatPassword">Repeat password</InputLabel>
+                    <OutlinedInput
+                        id="repeatPassword"
+                        error={repeatPassInput.trim().length < 4 && repeatPassInput.length !== 0}
+                        value={repeatPassInput}
+                        onChange={(ev) => changeInput(ev, 'repeatPassword')}
+                        label="Re-type password"
+                        variant="outlined"
+                        type={isPassVisible ? 'text' : 'password'}
+                        autoComplete="current-password"
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                                edge="end"
+                                >
+                                {isPassVisible ? <Visibility /> : <VisibilityOff />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                    />
+                </FormControl>
+                <TextField
+                    id="age"
+                    error={ageInput < 18 && ageInput !== ""}
+                    value={ageInput}
+                    onChange={(ev) => changeInput(ev, 'age')}
+                    label="How old are you?"
+                    variant="outlined"
+                    type="number"
+                />
                 <Button onClick={getInputData} variant="contained" color="primary">Register</Button>
             </form>
         </Container>
