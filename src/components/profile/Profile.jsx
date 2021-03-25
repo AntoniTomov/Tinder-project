2b5a1045fb79ed889cb165ca5f8fe51641a61a20
Vik <vkaraulanov@gmail.com>
2021-03-25 18:00:06 +0200

    added filereader and images container for displaying

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-
-import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
-import MenuIcon from '@material-ui/icons/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import { Link } from 'react-router-dom';
+import Fab from '@material-ui/core/Fab';
+import Tooltip from '@material-ui/core/Tooltip';
+import Zoom from '@material-ui/core/Zoom';
+import AddPhotoAlternateIcon from '@material-ui/icons/AddPhotoAlternate';
+
 import logo from '../menuAppBar/dinderTrans2.png'
 
 const profile = {
@@ -28,88 +27,94 @@ const profile = {
 };
 
 const useStyles = makeStyles((theme) => ({
-    logoPaper: {
-        margin: `${theme.spacing(1)}px auto`,
-        background: 'rgba(255,255,255,0.3)',
-        padding: '0 10px',
+    root: {
+        width: '50%',
+        margin: '0 auto',
     },
-    logo: {
-        width: '100px',
+    paper: {
+        background: 'rgba(255, 255, 255, 0.5)',
     },
-    appBar: {
-        background: 'none',
-        padding: `0 ${theme.spacing(3)}px`
+    item: {
+        border: '1px solid black'
     },
-    title: {
-        fontSize: '3rem',
+    button: {
+        color: '#e66465',
+        background: '#9198e5',
     },
-    menu: {
-        fontSize: '35px'
-    }
+    input: {
+        display: 'none'
+    },
+    img: {
+        width: '200px',
+    },
 }));
 
 
 
 const Profile = () => {
     const classes = useStyles();
+    let [images, setImages] = useState([]);
+    let [imagesCounter, setImagesCounter] = useState(0);
+    // let [selectedFile, setSelectedFile] = useState(null);
 
-    const [auth, setAuth] = React.useState(false);
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-    const handleMenu = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
+    const handleUploadClick = (e) => {
+        let data = e.target;
+        if (data.files && data.files[0]) {
+            var reader = new FileReader();
+            reader.readAsDataURL(data.files[0]);
 
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+            reader.onloadend = function (e) {
+                console.log('target', e.target);
+                setImages([...images, reader.result]);
+            }
+
+            setImagesCounter(prev => prev + 1);
+        }
+    }
 
     return (
         <>
             <CssBaseline />
-            <AppBar elevation={0} className={classes.appBar}>
-                <Grid
-                    container
-                    justify='space-between'
-                    alignItems='center'
-                >
-                    <Grid item xs={1}>
-                        <Paper className={classes.logoPaper} elevation={10}>
-                            <img src={logo} className={classes.logo} alt='logo'></img>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Typography variant="h4" className={classes.title}>
-                            DINDER!
-                        </Typography>
-                    </Grid>
-                    <Grid item>
-                        <MenuIcon className={classes.menu} aria-controls="simple-menu" aria-haspopup="true" onClick={handleMenu} />
-                        <Menu
-                            id="simple-menu"
-                            anchorEl={anchorEl}
-                            keepMounted
-                            open={Boolean(anchorEl)}
-                            onClose={handleClose}
+            <Grid
+                container
+                className={classes.root}
+                justify='center'
+                direction='column'
+            >
+                <Paper elevation={10} className={classes.paper}>
+                    <Grid container direction='column'>
+                        <Grid item xs className={classes.item}>
+                            <input
+                                accept="image/*"
+                                className={classes.input}
+                                id="contained-button-file"
+                                multiple
+                                type="file"
+                                onChange={e => { handleUploadClick(e) }}
+                            />
+                            <label htmlFor="contained-button-file">
+                                <Tooltip title="Add Photo" arrow TransitionComponent={Zoom} placement='left'>
+                                    <Fab component="span" size='medium' className={classes.button}>
+                                        <AddPhotoAlternateIcon />
+                                    </Fab>
+                                </Tooltip>
+                            </label>
+                        </Grid>
+                        <Grid
+                            container
+                            spacing={3}
                         >
-                            <MenuItem component={Link} to='/home'>Home</MenuItem>
-                            {
-                                true ?
-                                    <div>
-                                        <MenuItem component={Link} to='/matches'>Kotenca</MenuItem>
-                                        <MenuItem component={Link} to='/profile'>Profile</MenuItem>
-                                    </div>
-                                    :
-                                    <div>
-                                        <MenuItem component={Link} to='/register'>Register</MenuItem>
-                                        <MenuItem component={Link} to='/login'>Login</MenuItem>
-                                    </div>
-                            }
-                        </Menu>
+                            {images && images.map((img, i) => {
+                                return (
+                                    <Grid item key={i} xs={4}>
+                                        <img src={img} alt='userImage' className={classes.img} />
+                                    </Grid>
+                                )
+                            })}
+                        </Grid>
                     </Grid>
-
-                </Grid>
-            </AppBar>
+                </Paper>
+            </Grid>
         </>
     )
 }
