6d3ca913f447b1cc14c33736709aa45329b36178
Antoni Tomov <antotomov@gmail.com>
2021-03-26 17:41:12 +0200

    creating Chat.module.css, re-locating the mock-data in App.jsx, configuring the Chat to display the mock-data info

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -15,71 +15,91 @@ const useStyles = makeStyles(theme => ({
     }
     }));
 
-export default function ChosenMatch({user}) {
+export default function Chat({users}) {
 
     const [formValue, setFormValue] = useState('');
+    const [isPersonActive, setisPersonActive] = useState(false);
     const styles = useStyles();
+
+    
+    const toggleClass = (ev) => {
+        console.log(ev.target);
+        ev.target.className === 'active' ? ev.target.className = '' : ev.target.className = 'active';
+        console.log(ev.target);
+    }
         
     return (
         <CssBaseline>
             <div className={chatClasses.chatContainer}>
-                <main className={chatClasses.chat}>
-                    <div className={chatClasses.messageContainer}>
-                        <div className={[`${chatClasses.message} ${chatClasses.received}`]}>
-                            <Avatar className={styles.head} alt={user.name} src={user.url} />
-                            <p>dsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff</p>
-                            <span>12:30pm</span>
-                        </div>
-                        <div className={[`${chatClasses.message} ${chatClasses.sent}`]}>
-                            <Avatar className={styles.head} alt={user.name} src={user.url} />
-                            <p>Effffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff</p>
-                            <span>12:30pm</span>
-                        </div>
-                        <div className={[`${chatClasses.message} ${chatClasses.received}`]}>
-                                <Avatar className={styles.head} alt={user.name} src={user.url} />
-                                <p>Eto nqkuv message</p>
-                                <span>12:30pm</span>
-                        </div>
-                        <div className={[`${chatClasses.message} ${chatClasses.sent}`]}>
-                            <Avatar className={styles.head} alt={user.name} src={user.url} />
-                            <p>Eto nqkuv message</p>
-                            <span>12:30pm</span>
-                        </div>
-                        <div className={[`${chatClasses.message} ${chatClasses.received}`]}>
-                                <Avatar className={styles.head} alt={user.name} src={user.url} />
-                                <p>Eto nqkuv message</p>
-                                <span>12:30pm</span>
-                        </div>
-                        <div className={[`${chatClasses.message} ${chatClasses.sent}`]}>
-                            <Avatar className={styles.head} alt={user.name} src={user.url} />
-                            <p>Eto nqkuv message</p>
-                            <span>12:30pm</span>
-                        </div>
-                        <div className={[`${chatClasses.message} ${chatClasses.received}`]}>
-                                <Avatar className={styles.head} alt={user.name} src={user.url} />
-                                <p>Eto nqkuv message</p>
-                                <span>12:30pm</span>
-                        </div>
-                        <div className={[`${chatClasses.message} ${chatClasses.sent}`]}>
-                            <Avatar className={styles.head} alt={user.name} src={user.url} />
-                            <p>Eto nqkuv message</p>
-                            <span>12:30pm</span>
-                        </div>
-                        <div className={[`${chatClasses.message} ${chatClasses.received}`]}>
-                                <Avatar className={styles.head} alt={user.name} src={user.url} />
-                                <p>Eto nqkuv message</p>
-                                <span>12:30pm</span>
-                        </div>
-                        <div className={[`${chatClasses.message} ${chatClasses.sent}`]}>
-                            <Avatar className={styles.head} alt={user.name} src={user.url} />
-                            <p>Eto nqkuv message</p>
-                            <span>12:30pm</span>
+                    <div className={chatClasses.profilesInChat}>
+                        <div className={chatClasses.innerDivChat}>
+                            {users.map(user => 
+                                <span key={user.id} className={chatClasses.missedMsg} onClick={(ev) => toggleClass(ev)}>
+                                    <Avatar className={styles.head} alt={user.name} src={user.url} />
+                                    {user.name}
+                                </span>
+                            )}
                         </div>
                     </div>
-                    <form className={chatClasses.chatForm} onSubmit={() => console.log('cuknaha me')}>
-                        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
-                        <button type="submit" disabled={!formValue}>💕</button>
-                    </form>
+                <main className={chatClasses.chat}>
+                        <div className={chatClasses.messageContainer}>
+                            {users.map(user => 
+                                <div key={user.id} className={[`${chatClasses.message} ${chatClasses.received}`]}>
+                                    <Avatar className={styles.head} alt={user.name} src={user.url} />
+                                    <p>dsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff</p>
+                                    <span>12:30pm</span>
+                                </div>
+                            )}
+                            {/* <div className={[`${chatClasses.message} ${chatClasses.sent}`]}>
+                                <Avatar className={styles.head} alt={user.name} src={user.url} />
+                                <p>Effffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff</p>
+                                <span>12:30pm</span>
+                            </div>
+                            <div className={[`${chatClasses.message} ${chatClasses.received}`]}>
+                                    <Avatar className={styles.head} alt={user.name} src={user.url} />
+                                    <p>Eto nqkuv message</p>
+                                    <span>12:30pm</span>
+                            </div>
+                            <div className={[`${chatClasses.message} ${chatClasses.sent}`]}>
+                                <Avatar className={styles.head} alt={user.name} src={user.url} />
+                                <p>Eto nqkuv message</p>
+                                <span>12:30pm</span>
+                            </div>
+                            <div className={[`${chatClasses.message} ${chatClasses.received}`]}>
+                                    <Avatar className={styles.head} alt={user.name} src={user.url} />
+                                    <p>Eto nqkuv message</p>
+                                    <span>12:30pm</span>
+                            </div>
+                            <div className={[`${chatClasses.message} ${chatClasses.sent}`]}>
+                                <Avatar className={styles.head} alt={user.name} src={user.url} />
+                                <p>Eto nqkuv message</p>
+                                <span>12:30pm</span>
+                            </div>
+                            <div className={[`${chatClasses.message} ${chatClasses.received}`]}>
+                                    <Avatar className={styles.head} alt={user.name} src={user.url} />
+                                    <p>Eto nqkuv message</p>
+                                    <span>12:30pm</span>
+                            </div>
+                            <div className={[`${chatClasses.message} ${chatClasses.sent}`]}>
+                                <Avatar className={styles.head} alt={user.name} src={user.url} />
+                                <p>Eto nqkuv message</p>
+                                <span>12:30pm</span>
+                            </div>
+                            <div className={[`${chatClasses.message} ${chatClasses.received}`]}>
+                                    <Avatar className={styles.head} alt={user.name} src={user.url} />
+                                    <p>Eto nqkuv message</p>
+                                    <span>12:30pm</span>
+                            </div>
+                            <div className={[`${chatClasses.message} ${chatClasses.sent}`]}>
+                                <Avatar className={styles.head} alt={user.name} src={user.url} />
+                                <p>Eto nqkuv message</p>
+                                <span>12:30pm</span>
+                            </div> */}
+                        </div>
+                        <form className={chatClasses.chatForm} onSubmit={() => console.log('cuknaha me')}>
+                            <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+                            <button type="submit" disabled={!formValue}>💕</button>
+                        </form>
                 </main>
             </div>
         </CssBaseline>
