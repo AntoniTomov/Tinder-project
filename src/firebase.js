0bff822edefb8e603bed85a231236a9ccc6aa279
Vik <vkaraulanov@gmail.com>
2021-04-06 17:40:25 +0300

    hiding firebaseConfig data

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBTxAOZJx56d3i9hCiD5ZcYpnp4cvfady4",
-    authDomain: "fir-project-d9b09.firebaseapp.com",
-    projectId: "fir-project-d9b09",
-    storageBucket: "fir-project-d9b09.appspot.com",
-    messagingSenderId: "74333153272",
-    appId: "1:74333153272:web:ebfbec43f8b31327d1980d"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
