37f152e2d0b692102d5b65719c6c8a7e878bd1f1
Vik <vkaraulanov@gmail.com>
2021-04-06 15:39:09 +0300

    updated the placeholder in the chat to be the corresponding username

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,59 +1,36 @@
 import chatClasses from './Chat.module.css';
 import { useState, useEffect, useMemo, useRef } from "react";
-import Avatar from '@material-ui/core/Avatar';
-import { CssBaseline, makeStyles } from '@material-ui/core';
-import firebase, { db } from '../../firebase';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import { db } from '../../firebase';
 import { useSelector } from 'react-redux';
 import ChatHead from './chatHead';
 import { sentMessage } from './chatService';
 import Message from './Message';
 
-
-const useStyles = makeStyles(theme => ({
-    head: {
-        width: theme.spacing(4),
-        height: theme.spacing(4),
-        margin: theme.spacing(1),
-    },
-    timestamp: {
-        color: 'gray',
-        fontSize: '8px',
-    }
-}));
-
 export default function Chat() {
-    const styles = useStyles();
     const user = useSelector(state => state.currentUser);
+    const allUsers = useSelector(state => state.allUsers);
 
     const [formValue, setFormValue] = useState('');
     const [allChats, setAllChats] = useState([]);
     const [targetChatId, setTargetChatId] = useState('');
+    const [selectedChatUsername, setSelectedChatUsername] = useState('');
     const dummyDiv = useRef();
-    // const [lastChatChangedId, setLastChatChangedId] = useState('');
-    // const [updatedChats, setUpdatedChats] = useState([]);
 
     const chatRoomsRef = db.collection('chatRooms').where('users', 'array-contains', user.uid);
 
     // Fetch all chat rooms
     useEffect(() => {
-        /* gledame za promeni v qnkoi ot tezi documenti */
         chatRoomsRef.onSnapshot(snapShot => {
-            // console.log('snapshot=> ', snapShot)
             const tempAllChats = []
             snapShot.forEach(doc => {
                 const data = doc.data();
                 let chat = { ...data, id: doc.id }
-                
-                // tempAllChats.push(chat);
                 chat.messages.length !== 0 ? tempAllChats.unshift(chat) : tempAllChats.push(chat);
             })
-
             const sortedByTimeChats = tempAllChats.sort((a, b) => b.lastMessageTimestamp - a.lastMessageTimestamp);
-            setAllChats(sortedByTimeChats)
-
-            // if(sortedByTimeChats.length > 0) {
-            //     setTargetChatId(sortedByTimeChats[0].id)
-            // }
+            setAllChats(sortedByTimeChats);
         })
     }, [])
 
@@ -68,12 +45,10 @@ export default function Chat() {
 
     const sendMessage = async (e) => {
         e.preventDefault();
-
         await sentMessage(targetChatId, user.uid, formValue);
         console.log('izpratih ei tui: ', formValue, 'v chata: ', targetChatId, 'ot: ', user.uid);
         setFormValue('');
 
-        // scroll to bottom -> useRef()
         dummyDiv.current.scrollIntoView({
             behavior: "smooth",
             block: "nearest",
@@ -81,8 +56,9 @@ export default function Chat() {
         });
     }
 
-    const selectTargetChat = (id) => {
+    const selectTargetChat = (id, targetUser) => {
         setTargetChatId(id);
+        setSelectedChatUsername(targetUser.name)
     }
 
     const currentChat = useMemo(() => {
@@ -90,7 +66,6 @@ export default function Chat() {
         return allChats[id];
     }, [targetChatId, allChats])
 
-    // console.log('currentChat =>>> ', currentChat)
     return (
         <CssBaseline>
             <div className={chatClasses.chatContainer}>
@@ -98,19 +73,18 @@ export default function Chat() {
                     <div className={chatClasses.innerDivChat}>
                         {
                             allChats.map(chat => {
-                                return (<ChatHead 
+                                return (<ChatHead
                                     chat={chat}
                                     isActive={targetChatId === chat.id}
                                     selectTargetChat={selectTargetChat}
                                     key={chat.id}
-                                    />)
+                                />)
                             })
                         }
                     </div>
                 </div>
                 <main className={chatClasses.chat}>
                     <div className={chatClasses.messageContainer}>
-
                         {
                             currentChat &&
                             currentChat.messages.map(message => {
@@ -120,8 +94,12 @@ export default function Chat() {
                         <div ref={dummyDiv} />
                     </div>
                     <form className={chatClasses.chatForm} onSubmit={sendMessage}>
-                        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
-                        <button type="submit" disabled={!currentChat || !formValue}>💕</button>
+                        <input value={formValue}
+                        onChange={(e) => setFormValue(e.target.value)}
+                        placeholder={currentChat ? `Chat with ${selectedChatUsername}` : ''} />
+                        <button type="submit" disabled={!currentChat || !formValue}>
+                            <FavoriteIcon />
+                        </button>
                     </form>
                 </main>
             </div>
